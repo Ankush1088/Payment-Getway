d599c53139fa408ef559ce40dc6aa33cbb71ecab
Ankush <ankushsingh1088@gmail.com>
2025-03-18 22:13:41 +0530

    Update productController.js

--- server-side/Controller/productController.js
+++ server-side/Controller/productController.js
@@ -1,5 +1,5 @@
 import { instance } from "../index.js";
-import crypto from 'crypto';
+import crypto from "crypto";
 
 export const processPayment = async (req, res) => {
   try {
@@ -9,11 +9,7 @@ export const processPayment = async (req, res) => {
     };
 
     const order = await instance.orders.create(options);
-    res.status(200).json({
-      success: true,
-      message: 'Payment Processed Successfully!',
-      order,
-    });
+    res.status(200).json({ success: true, message: "Payment Processed Successfully!", order });
   } catch (error) {
     console.error("Payment Processing Error:", error);
     res.status(500).json({ success: false, message: "Payment Processing Failed" });
@@ -21,14 +17,13 @@ export const processPayment = async (req, res) => {
 };
 
 export const getKey = async (req, res) => {
-  res.status(200).json({
-    key: process.env.RAZORPAY_API_KEY,
-  });
+  res.status(200).json({ key: process.env.RAZORPAY_API_KEY });
 };
 
 export const paymentVerification = async (req, res) => {
-   console.log("Payment Verification Triggered");
+  console.log("Payment Verification Triggered");
   console.log("Received Body:", req.body);
+
   const { razorpay_payment_id, razorpay_order_id, razorpay_signature } = req.body;
   const body = razorpay_order_id + "|" + razorpay_payment_id;
 
@@ -41,8 +36,7 @@ export const paymentVerification = async (req, res) => {
   console.log("Verification Status:", expectedSignature === razorpay_signature);
 
   if (expectedSignature === razorpay_signature) {
-    //redirect
-    return res.json(`${process.env.FRONTEND_URL}/PaymentsSuccess?reference=${razorpay_payment_id}`);
+    res.json({ success: true, message: "Payment Verified", reference: razorpay_payment_id });
   } else {
     res.status(400).json({ success: false, message: "Invalid Signature" });
   }
